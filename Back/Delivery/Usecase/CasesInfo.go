f6b9dd888b039220ca532adf192b8f8e6900a81b
NunChatSpace <77067491+NunChatSpace@users.noreply.github.com>
2021-05-03 22:24:11 +0700

    Made it filterable

--- Back/Delivery/Usecase/CasesInfo.go
+++ Back/Delivery/Usecase/CasesInfo.go
@@ -3,7 +3,6 @@ package Usecase
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"mainmodule/Database"
 	"mainmodule/Delivery"
@@ -22,11 +21,20 @@ type cases struct {
 
 func (c *cases) GetData(ctx *fiber.Ctx) {
 	resp := Model.ResponseModel{}
+	body := new(Model.Info)
+	bodyParseError := ctx.BodyParser(body)
+	if bodyParseError != nil {
+		resp = Model.ResponseModel{
+			Status:  http.StatusInternalServerError,
+			Message: bodyParseError.Error(),
+		}
+		ctx.JSON(resp)
+	}
+	filter := c.makeFilter(body)
 	context, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	cursor, err := c.DB.CasesInfoCollection.Find(context, bson.D{{}})
-	fmt.Printf("%v \n", context)
+	cursor, err := c.DB.CasesInfoCollection.Find(context, filter)
+	// fmt.Printf("%v \n", context)
 	if err != nil {
-		log.Fatal(err.Error())
 		resp = Model.ResponseModel{
 			Status:  http.StatusInternalServerError,
 			Message: err.Error(),
@@ -59,6 +67,52 @@ func (c *cases) GetData(ctx *fiber.Ctx) {
 	}
 
 	ctx.JSON(resp)
+}
+
+func (c *cases) makeFilter(m *Model.Info) bson.M {
+	query := bson.M{}
+	// bson.D{{"foo", "bar"}, {"hello", "world"}, {"pi", 3.14159}}
+	if m.ConfirmDate != "" {
+		query["confirmdate"] = m.ConfirmDate
+	}
+	if m.No != "" {
+		query["no"] = m.No
+	}
+	if m.Age != 0 {
+		query["age"] = m.Age
+	}
+	if m.Gender != "" {
+		query["gender"] = m.Gender
+	}
+	if m.GenderEn != "" {
+		query["genderen"] = m.GenderEn
+	}
+	if m.Nation != "" {
+		query["nation"] = m.Nation
+	}
+	if m.NationEn != "" {
+		query["nationen"] = m.NationEn
+	}
+	if m.Province != "" {
+		query["province"] = m.Province
+	}
+	if m.ProvinceId != 0 {
+		query["provinceid"] = m.ProvinceId
+	}
+	if m.District != "" {
+		query["district"] = m.District
+	}
+	if m.ProvinceEn != "" {
+		query["provinceen"] = m.ProvinceEn
+	}
+	if m.Detail != "" {
+		query["detail"] = m.Detail
+	}
+	if m.StatQuarantine != 0 {
+		query["statquarantine"] = m.StatQuarantine
+	}
+	// fmt.Println(query)
+	return query
 }
 
 func NewCasesInfo() Domain.CasesInfoInterface {
